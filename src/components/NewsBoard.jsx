5b70b5f54dccd0b4d161037f142f4e3880888dfa
Sameera Wijesekara <sameerawijesekara2001@gmail.com>
2025-02-18 12:36:40 +0530

    project init

---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewsBoard = () => {
+  return (
+    <div>
+    <h2 className='text-center'><span className=''>Latest News</span></h2>
+      
+    </div>
+  )
+}
+
+export default NewsBoard
